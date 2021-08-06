3905c7cff04bc2c335c3a8b7d25a3a1365586eeb
Tran Nghia <trantuannghia24501@gmail.com>
2021-08-06 23:12:14 +0700

    footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,15 +4,17 @@
     <v-main>
       <router-view/>
     </v-main>
+    <Footer />
   </v-app>
 </template>
-
 <script>
 import Header from '@/components/Header.vue'
-
+import Footer from '@/components/Footer.vue'
 export default {
   components: {
-    Header
+    Header,
+    Footer
+
   },
   name: 'App',
 
