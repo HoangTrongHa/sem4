f2f334196c6a10d274d37e27c27f62fde7767641
Tran Nghia <trantuannghia24501@gmail.com>
2021-08-10 18:08:26 +0700

    fix checkout

--- frontend/src/components/ProductItemSuccess.vue
+++ frontend/src/components/ProductItemSuccess.vue
@@ -1,58 +1,45 @@
 <template>
-    <div class="wrap-table-products-item">
-        <table class="table-products__item" style="margin-bottom: 5px"
-            v-for="(items,index) of getDataOrder" :key="index"
-        >
-            <thead>
-                <tr>
-                    <th>Tên sản phẩm</th>
-                    <th>Số lượng</th>
-                    <th width="120px">Giá niêm yết</th>
-                    <th>Biến thể</th>
-                    <th>Thành tiền</th>
-                </tr>
-            </thead>
-            <tbody v-for="(item,index) of items.item_cart" :key="index">                                                                                                                        <tr class="table-products__item-payment">
-                <td class="infor-product">{{ item.name }}</td>
-                <td class="infor-product">1</td>
-                <td class="infor-product">289.000đ</td>
-                <td class="infor-product">
-                    Đen/L                                        
-                </td>
-                <td class="infor-product">289.000đ</td>
-            </tr>                           
-            </tbody>
-        </table>
-        <table class="table-product"
-         v-for="(items,index) of getDataOrder" :key="index"
-        >
-            <tbody>
-                <tr>
-                    <td class="left">Mã Giảm Giá</td>
-                    <td class="right"></td>
-                </tr>
-                <tr>
-                    <td class="left">Tổng giá trị sản phẩm	</td>
-                    <td class="right"> {{ items.total }} đ</td>
-                </tr>
-                <tr>
-                    <td class="left">Phí giao hàng</td>
-                    <td class="right">0</td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="wrap-infor-order"
-            v-for="(items,index) of getDataOrder" :key="index"
-        >
-            <div class="title">Thông tin khách hàng </div>
-            <div class="wrap-infor">
-            <div>Tên người nhận: {{ items.userName }}</div>
-            <div>Email: {{ items.email }}</div>
-            <div>Số điện thoại: {{ items.phoneNumber }}</div>
-            <div>Hình thức thanh toán: Thanh toán khi nhận hàng {{ items.option }}</div>
+    <v-container id="success_code">
+            <div class="box">
+                <div class="title">
+                    <h3>Đặt Hàng Thành Công!</h3>
+                    <span class="detail">Đơn hàng của bạn đã được gửi tới hệ thống, nhân viên chúng tôi đang xử lí đơn hàng này</span>
+                </div>
+                <hr>
+                <div class="info-user">
+                    <div class="name">Tên người nhận:<span class="info-content">Nghia</span></div>
+                    <div class="name">Email:<span class="info-content">nghia@gmail.com</span></div>
+                    <div class="name">Địa chỉ nhận hàng:<span class="info-content">Ha noi</span></div>
+                    <div class="name">Phương thức thanh toán:<span class="info-content">Cod</span></div>
+                </div>
+                <hr>
+                <div class="product">
+                        <img src="http://yvanhien.com/wp-content/uploads/bfi_thumb/DSC1561-o6vcje8aeu7zm7cbrg8zv0lfracejlawsapdybpijw.jpg" alt="">
+                        <div class="name">Áo dài nam năm thân cổ đứng </div>
+                        <div class="qty">Số lượng: <div class="qty-number">1</div></div>
+                        <div class="subtotal">Tạm tính: <div class="subtotal-price">$199.00</div></div>    
+                </div>
+                <hr>
+                <div class="price">
+                    <div class="price-content">
+                        <div class="subtotal">Tạm tính: $199.00</div>
+                        <div class="ship">Phí giao hàng: 0</div>
+                        <div class="discount">Giảm giá: 0</div>
+                    </div>
+                </div>
+                <hr>
+                <div class="total">
+                    <div class="total-content">Total: <div class="total-price">$199.00</div></div>
+                </div>
+                <hr>
+                <div class="thank">
+                    <div class="thank-title"> <i>Chúng tôi sẽ gửi cho bạn email xác nhận của đơn hàng. Hy vọng nhận được sự phản hồi của bạn sau khi nhận hàng.</i></div>
+                    <div class="thank-content"><i>Thank you!</i></div>                  
+                </div>
+                <hr >
+                <div class="right">Cổ Phục Việt Nam</div>
             </div>
-        </div>
-    </div>
+        </v-container>
 </template>
 
 <script>
@@ -70,49 +57,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.wrap-table-products-item {
-    width: 100%;
-    .table-products__item {
-        width: 100%;
-        margin: 50px 0;
-    }
-    .infor-product {
-        text-align: center;
-        padding: 10px 0;
-    }
-    .table-product {
-        width: 100%;
-        tbody {
-            tr {
-                .left {
-                    font-style: italic;
-                    background-color: #f2f2f2;
-                    padding: 10px 15px;
-                    text-align: left;
-                }
-                .right {
-                    font-style: italic;
-                    background-color: #f2f2f2;
-                    padding: 10px 15px;
-                    text-align: right;
-                }
-            }
-        }
-
-    }
-    .wrap-infor-order {
-        width: 100%;
-        margin: 100px 0;
-        .title {
-            width: 100%;
-            text-align: center;
-        }
-        .wrap-infor {
-            div {
-                padding: 15px 0;
-                font-style: italic;
-            }
-        }
-    }
-}
+    @import "@/scss/checkout-success.scss";
 </style>
--- frontend/src/views/CheckOut/success/index.vue
+++ frontend/src/views/CheckOut/success/index.vue
@@ -1,46 +1,8 @@
 d<template>
     <div>
-        <v-container id="success_code">
-            <div class="box">
-                <div class="title">
-                    <h3>Đặt Hàng Thành Công!</h3>
-                    <span class="detail">Đơn hàng của bạn đã được gửi tới hệ thống, nhân viên chúng tôi đang xử lí đơn hàng này</span>
-                </div>
-                <hr>
-                <div class="info-user">
-                    <div class="name">Tên người nhận:<span class="info-content">Nghia</span></div>
-                    <div class="name">Email:<span class="info-content">nghia@gmail.com</span></div>
-                    <div class="name">Địa chỉ nhận hàng:<span class="info-content">Ha noi</span></div>
-                    <div class="name">Phương thức thanh toán:<span class="info-content">Cod</span></div>
-                </div>
-                <hr>
-                <div class="product">
-                        <img src="http://yvanhien.com/wp-content/uploads/bfi_thumb/DSC1561-o6vcje8aeu7zm7cbrg8zv0lfracejlawsapdybpijw.jpg" alt="">
-                        <div class="name">Áo dài nam năm thân cổ đứng </div>
-                        <div class="qty">Số lượng: <div class="qty-number">1</div></div>
-                        <div class="subtotal">Tạm tính: <div class="subtotal-price">$199.00</div></div>    
-                </div>
-                <hr>
-                <div class="price">
-                    <div class="price-content">
-                        <div class="subtotal">Tạm tính: $199.00</div>
-                        <div class="ship">Phí giao hàng: 0</div>
-                        <div class="discount">Giảm giá: 0</div>
-                    </div>
-                </div>
-                <hr>
-                <div class="total">
-                    <div class="total-content">Total: <div class="total-price">$199.00</div></div>
-                </div>
-                <hr>
-                <div class="thank">
-                    <div class="thank-title"> <i>Chúng tôi sẽ gửi cho bạn email xác nhận của đơn hàng. Hy vọng nhận được sự phản hồi của bạn sau khi nhận hàng.</i></div>
-                    <div class="thank-content"><i>Thank you!</i></div>                  
-                </div>
-                <hr >
-                <div class="right">Cổ Phục Việt Nam</div>
-            </div>
-        </v-container>
+        <ProductItemSuccess 
+                    :getDataOrder="getDataOrder"
+                />
     </div>
 </template>
 
@@ -67,7 +29,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-    @import "@/scss/checkout-success.scss";
-</style>
 
