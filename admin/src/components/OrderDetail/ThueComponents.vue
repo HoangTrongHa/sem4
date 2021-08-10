c6d87189b9e20b3c4afedc40ee5303b0acb35ed0
Tran Nghia <trantuannghia24501@gmail.com>
2021-08-10 23:13:32 +0700

    order-detail

--- admin/src/components/OrderDetail/ThueComponents.vue
+++ admin/src/components/OrderDetail/ThueComponents.vue
@@ -1,107 +1,118 @@
 <template>
-    <div>
-        <table class="table mb-0">
-        <thead>
-            <tr class="text-muted">
-            <th scope="col">Tên Sản Phẩm</th>
-            <th scope="col" class="text-right">Số Lượng</th>
-            <th scope="col" class="text-right">Giá Thuê</th>
-            <th scope="col" class="text-right">Giá Cọc</th>
-            <th scope="col" class="text-right">Ngày Thuê</th>
-            <th scope="col" class="text-right">Ngày Trả</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item,key) of dataBy" :key="key">
-                <td>
-                    <div class="active-project d-flex align-items-center mt-0 ">
-                        <div class="h-avatar is-medium">
-                            <img class="avatar rounded" :src="item.img">
-                        </div>
-                        <div class="data-content">
-                            <div>
-                            <span class="font-weight-bold">{{ item.name }}</span>                           
-                            </div>
-                        </div>
-                    </div>
-                </td>
-                <td class="text-right">
-                    {{ item.qtyCus }}
-                </td>
-                <td class="text-right">
-                    {{ item.price }}
-                </td>
-                <td class="text-right">
-                    {{ item.deposit_price }}
-                </td>
-                <td class="text-right">
-                    {{ item.startDay }}
-                </td>
-                <td class="text-right">
-                    {{ item.endDay }}
-                </td>
-            </tr>    
-        </tbody>
-        </table>
-        <div class="wrapTotalBy">
-            <div class="title">Tổng Tiền Cọc: </div>
-            <div class="value">{{ calcCoc }} Đồng</div>
-        </div>
-        <div class="wrapTotalBy">
-            <div class="title">Tổng Tiền Thuê: </div>
-            <div class="value">{{ calcThue }} Đồng</div>
-        </div>
+  <div>
+    <div class="status">
+      <v-select
+        :items="items"
+        label="Chuyển Trạng Thái"
+        outlined
+        v-model="status"
+      ></v-select>
     </div>
+    <table class="table mb-0">
+      <thead>
+        <tr class="text-muted">
+          <th scope="col">Tên Sản Phẩm</th>
+          <th scope="col" class="text-right">Số Lượng</th>
+          <th scope="col" class="text-right">Giá Thuê</th>
+          <th scope="col" class="text-right">Giá Cọc</th>
+          <th scope="col" class="text-right">Ngày Thuê</th>
+          <th scope="col" class="text-right">Ngày Trả</th>
+          <th scope="col" class="text-right">Trạng thái</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, key) of dataBy" :key="key">
+          <td>
+            <div class="active-project d-flex align-items-center mt-0 ">
+              <div class="h-avatar is-medium">
+                <img class="avatar rounded" :src="item.img" />
+              </div>
+              <div class="data-content">
+                <div>
+                  <span class="font-weight-bold">{{ item.name }}</span>
+                </div>
+              </div>
+            </div>
+          </td>
+          <td class="text-right">
+            {{ item.qtyCus }}
+          </td>
+          <td class="text-right">
+            {{ item.price }}
+          </td>
+          <td class="text-right">
+            {{ item.deposit_price }}
+          </td>
+          <td class="text-right">
+            {{ item.startDay }}
+          </td>
+          <td class="text-right">
+            {{ item.endDay }}
+          </td>         
+        </tr>
+      </tbody>
+    </table>
+    <div class="wrapTotalBy">
+      <div class="title">Tổng Tiền Cọc:</div>
+      <div class="value">{{ calcCoc }} VNĐ</div>
+    </div>
+    <div class="wrapTotalBy">
+      <div class="title">Tổng Tiền Thuê:</div>
+      <div class="value">{{ calcThue }} VNĐ</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props : {
-        dataBy : {
-            type : Array,
-            default : () => []
-        },
-        calcThue: Number,
-        calcCoc: Number
+  props: {
+    dataBy: {
+      type: Array,
+      default: () => [],
     },
-    data() {
-        return {
-        };
-    },
-    computed: {
-        
+    calcThue: Number,
+    calcCoc: Number,
   },
-}
+  data() {
+    return {};
+  },
+  computed: {},
+};
 </script>
 
 <style lang="scss" scoped>
-    .table {
-        width: 100%;
-        tbody {
-            tr {
-                td {
-                    .active-project {
-                        .h-avatar{ 
-                            margin: 0 10px 0 0;
-                            .avatar {
-                                height: 70px;
-                                width: 70px;
-                            }
-                        }
-                    }
-                }
+.status{
+    float: right;
+    width: 30%;
+    height: min-content;
+}
+.table {
+  width: 100%;
+  tbody {
+    tr {
+      td {
+        .active-project {
+          .h-avatar {
+            margin: 0 10px 0 0;
+            .avatar {
+              height: 70px;
+              width: 70px;
             }
+          }
         }
+      }
     }
-    .wrapTotalBy {
-        display: flex;
-        width: 100%;
-        justify-content: flex-end;
-        align-items: center;
-        .value {
-            font-size: 32px;
-            font-weight: bolder;
-            margin: 0 0 0 20px;
-        }
-    }
+  }
+}
+.wrapTotalBy {
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  .value {
+    font-size: 32px;
+    font-weight: bolder;
+    margin: 0 0 0 20px;
+  }
+}
 </style>
--- admin/src/page/OrderDetail/index.vue
+++ admin/src/page/OrderDetail/index.vue
@@ -1,258 +1,273 @@
 <template>
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Chi Tiết Đơn Hàng #{{ getOrderDetail.code_order }}</h1>
-        <v-row>
-            <h3>Đơn Mua</h3>
-            <div class="wrapBuy">
-                <div class="wrapInforUser">
-                    <div class="title">
-                        Thông tin người mua đơn hàng
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Họ Tên Người Mua</div>
-                        <div class="content">{{ getOrderDetail.user_register }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Họ Tên Người Nhận</div>
-                        <div class="content">{{ getOrderDetail.userName }}</div>
-                    </div>
-                     <div class="wrapInfor">
-                        <div class="title">Số Điện Thoại</div>
-                        <div class="content">{{ getOrderDetail.phoneNumber }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Email</div>
-                        <div class="content">{{ getOrderDetail.email }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Địa Chỉ Nhận Hàng</div>
-                        <div class="content">{{ getOrderDetail.address }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Thông Tin Nơi Ở</div>
-                        <div class="content">Tỉnh/Thành Phố {{ getOrderDetail.city }}</div>
-                        <div class="content">Quận/Huyện {{ getOrderDetail.district }}</div>
-                        <div class="content">Phường/Xã {{ getOrderDetail.ward }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Khách Hàng Ghi Chú</div>
-                        <div class="content">{{ getOrderDetail.note }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Trạng Thái</div>
-                        <div class="content">
-                            <v-select
-                                :items="items"
-                                label="Chuyển Trạng Thái"
-                                outlined
-                                v-model="status"
-                            ></v-select>
-                        </div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Hình Thức Thanh Toán</div>
-                        <div class="content">{{ getOrderDetail.option }}</div>
-                    </div>
-                    <div class="wrapInfor">
-                        <div class="title">Chức Năng</div>
-                        <div class="content">
-                            <v-btn
-                                tile
-                                :disabled="disable"
-                                color="success"
-                                class="button"
-                                @click="updateCart"
-                                :loading="loading"
-                            >
-                                <v-icon left>
-                                mdi-pencil
-                                </v-icon>
-                                Cập Nhật Đơn Hàng
-                            </v-btn>
-                        </div>
-                    </div>
-                </div>
-                <div class="wrapGroupOption">
-                    <div class="wrapByItem">
-                    <div class="title">Danh sách Đơn Hàng Mua</div>
-                        <div 
-                            class="wrapItemGroup" 
-                            v-if="countLenghtBuy === 0 "
-                        >
-                            Khong co don hang
-                        </div>
-                        <div v-else class="wrapItemGroup">
-                            <BuyComponents :dataBy="this.getProductItem.buy" />
-                        </div>
-                    </div>
-                    <div class="wrapThueItem">
-                    <div class="title">Danh sách Đơn Hàng Mua</div>
-                        <div 
-                            class="wrapItemGroup" 
-                            v-if="countLenghtThue === 0 "
-                        >
-                            Khong co don hang
-                        </div>
-                        <div v-else class="wrapItemGroup">
-                            <ThueComponents 
-                                :dataBy="this.getProductItem.thue" 
-                                :calcThue="calcThue"
-                                :calcCoc="calcCoc"
-                            />
-                        </div>
-                    </div>
-                </div>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4">Chi Tiết Đơn Hàng</h1>
+    <v-row>
+      <div class="order-detail">
+        <div class="info-detail">
+          <div class="order-info">
+            <h3>Thông tin đơn hàng</h3>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">ID</div>
+              <div class="data">#{{ getOrderDetail.code_order }}</div>
             </div>
-        </v-row>  
-    </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Thời gian đặt hàng</div>
+              <div class="data">{{ getOrderDetail.create_at }}</div>
+            </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Hình thức thanh toán</div>
+              <div class="data">{{ getOrderDetail.option }}</div>
+            </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Ghi chú của khách hàng</div>
+              <div class="data">{{ getOrderDetail.note }}</div>
+            </div>
+            <div class="order-detail-item">
+                <div class="order-detail-item-title">Trạng thái đơn hàng</div>
+              <div class="status">
+                <v-select
+                  id="status"
+                  :items="items"
+                  label="Chuyển Trạng Thái"
+                  outlined
+                  v-model="status"
+                >
+                </v-select>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div class="user-info">
+            <h3>Thông tin khách hàng</h3>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Tên người đặt hàng</div>
+              <div class="data">{{ getOrderDetail.user_register }}</div>
+            </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Tên người nhận hàng</div>
+              <div class="data">{{ getOrderDetail.userName }}</div>
+            </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Số điện thoại</div>
+              <div class="data">{{ getOrderDetail.phoneNumber }}</div>
+            </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Email</div>
+              <div class="data">{{ getOrderDetail.email }}</div>
+            </div>
+            <div class="order-detail-item">
+              <div class="order-detail-item-title">Địa chỉ nhận hàng</div>
+              <div class="data">
+                {{ getOrderDetail.address }}/{{ getOrderDetail.ward }}/{{
+                  getOrderDetail.district
+                }}/{{ getOrderDetail.city }}
+              </div>
+            </div>          
+          </div>
+        </div>
+        <div class="product">
+          <div class="wrapByItem">
+            <div class="title">Danh sách Đơn Hàng Mua</div>
+            <div class="wrapItemGroup" v-if="countLenghtBuy === 0">
+              Khong co don hang
+            </div>
+            <div v-else class="wrapItemGroup">
+              <BuyComponents :dataBy="this.getProductItem.buy" />
+            </div>
+          </div>
+          <hr>
+          <div class="wrapThueItem">
+            <div class="title">Danh sách Đơn Hàng Thuê</div>
+            <div class="wrapItemGroup" v-if="countLenghtThue === 0">
+              Khong co don hang
+            </div>
+            <div v-else class="wrapItemGroup">
+              <ThueComponents
+                :dataBy="this.getProductItem.thue"
+                :calcThue="calcThue"
+                :calcCoc="calcCoc"
+              />
+            </div>
+          </div>
+          <div class="updateButton">
+            <v-btn
+              tile
+              :disabled="disable"
+              color="success"
+              class="button"
+              @click="updateCart"
+              :loading="loading"
+            >
+              <v-icon left>
+                mdi-pencil
+              </v-icon>
+              Cập Nhật Đơn Hàng
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </v-row>
+  </div>
 </template>
 
 <script>
-import BuyComponents from "@/components/OrderDetail/BuyComponents.vue"
-import ThueComponents from "@/components/OrderDetail/ThueComponents.vue"
-import Service from '../../business/index'
+import BuyComponents from "@/components/OrderDetail/BuyComponents.vue";
+import ThueComponents from "@/components/OrderDetail/ThueComponents.vue";
+import Service from "../../business/index";
 
 export default {
-    data () {
-        return {
-           status: '',
-           items: [
-                {
-                id: 1,
-                text: 'Chờ Xác Nhận',
-               
-                },
-                {
-                id: 2,
-                text: 'Đơn Hàng Bị Hủy',
-               
-                },
-                {
-                id: 3,
-                text: 'Chờ Vận Chuyển',
-              
-                },
-                {
-                id: 4,
-                text: 'Đang Vận Chuyển',
-                },
-                {
-                    id: 5,
-                    text: 'Đã Tới Điểm Giao',
-                },
-                {
-                    id: 6,
-                    text: 'Hết Hạn Thuê',
-                },
-                {
-                    id: 7,
-                    text: 'Hoàn Thành',
-                },
-            ],
-            disable: Boolean,
-            getStatus: '',
-            loading: false
-        }
-    },
-    props : {
-        getOrder : {
-        type : Array,
-        default : () => []
-        }
-    },
-    components : {
-        BuyComponents,
-        ThueComponents
-    },
-    computed: {
-        getOrderDetail() {
-            return this.$store.state.order.find(item => item.id === this.$route.params.params)
+  data() {
+    return {
+      status: "",
+      items: [
+        {
+          id: 1,
+          text: "Chờ Xác Nhận",
         },
-        getProductItem() {
-            return JSON.parse(this.getOrderDetail.item_cart)
+        {
+          id: 2,
+          text: "Đơn Hàng Bị Hủy",
         },
-        countLenghtThue() {
-            return this.getProductItem.thue.length
+        {
+          id: 3,
+          text: "Chờ Vận Chuyển",
         },
-        countLenghtBuy() {
-            return this.getProductItem.buy.length
+        {
+          id: 4,
+          text: "Đang Vận Chuyển",
         },
-        calcThue() {
-            let total = 0;
-            this.getProductItem.thue.forEach((item) => {
-                total += item.price * item.qtyCus;
-            });
-            return total;
+        {
+          id: 5,
+          text: "Đã Tới Điểm Giao",
         },
-        calcCoc() {
-            let total = 0;
-            this.getProductItem.buy.forEach((item) => {
-                total += item.price * item.qtyCus;
-            });
-            return total;
+        {
+          id: 6,
+          text: "Hết Hạn Thuê",
         },
+        {
+          id: 7,
+          text: "Hoàn Thành",
+        },
+      ],
+      disable: Boolean,
+      getStatus: "",
+      loading: false,
+    };
+  },
+  props: {
+    getOrder: {
+      type: Array,
+      default: () => [],
     },
-    methods: {
-        updateCart() {
-            var data = this.getOrderDetail
-            data.status = this.status
-            console.log(data);
-            this.loading = true
-            Service.editOrder(data.id,data).then((response) => {
-                console.log(response.data);
-                this.loading = false
-            })
-            
-        }
+  },
+  components: {
+    BuyComponents,
+    ThueComponents,
+  },
+  computed: {
+    getOrderDetail() {
+      return this.$store.state.order.find(
+        (item) => item.id === this.$route.params.params
+      );
     },
-    created() {
-        this.status = this.$store.state.order.find(item => item.id === this.$route.params.params).status
-        this.disable = true
-        this.getStatus = this.$store.state.order.find(item => item.id === this.$route.params.params).status
+    getProductItem() {
+      return JSON.parse(this.getOrderDetail.item_cart);
     },
-    
-    watch: {
-        status:  function (val) {
-            if (val !== this.getStatus ) {
-                this.disable = false
-            } else {
-                console.log(2);
-            }
-        }
-    }
-}
+    countLenghtThue() {
+      return this.getProductItem.thue.length;
+    },
+    countLenghtBuy() {
+      return this.getProductItem.buy.length;
+    },
+    calcThue() {
+      let total = 0;
+      this.getProductItem.thue.forEach((item) => {
+        total += item.price * item.qtyCus;
+      });
+      return total;
+    },
+    calcCoc() {
+      let total = 0;
+      this.getProductItem.buy.forEach((item) => {
+        total += item.price * item.qtyCus;
+      });
+      return total;
+    },
+  },
+  methods: {
+    updateCart() {
+      var data = this.getOrderDetail;
+      data.status = this.status;
+      console.log(data);
+      this.loading = true;
+      Service.editOrder(data.id, data).then((response) => {
+        console.log(response.data);
+        this.loading = false;
+      });
+    },
+  },
+  created() {
+    this.status = this.$store.state.order.find(
+      (item) => item.id === this.$route.params.params
+    ).status;
+    this.disable = true;
+    this.getStatus = this.$store.state.order.find(
+      (item) => item.id === this.$route.params.params
+    ).status;
+  },
+
+  watch: {
+    status: function(val) {
+      if (val !== this.getStatus) {
+        this.disable = false;
+      } else {
+        console.log(2);
+      }
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-   .wrapBuy {
-        display: flex;
-        .wrapInforUser {
-            width: 45%;
-            background: #f9fbfe;
-            padding: 30px;
-            border-radius: 5px ;
-            .title {
-                font-weight: bolder;
-            }
-            .wrapInfor { 
-                width: 100%;
-                padding: 10px 0;
-                font-size: 22px;
-                display: flex;
-                justify-content: space-between;
-            }
-        }
-        .wrapGroupOption {
-            width: 55%;
-            padding:0 100px;
-            .wrapByItem {
-                width: 100%;
-            }
-        }
-    }
+.order-detail {
+  display: flex;
+  justify-content: space-between;
 
-  .button {
-    background: #4caf50 !important;
-    color: #fff;
+  .info-detail {
+    width: 35%;
+    padding: 20px;
+    line-height: 50px;
+    .order-info,
+    .user-info {
+      display: flex;
+      flex-direction: column;
+      h3 {
+        width: 100%;
+      }
+      .order-detail-item {
+        display: flex;
+        justify-content: space-between;
+        .order-detail-item-title {
+          width: max-content;
+          color: gray;
+        }
+        .data {
+          width: 60%;
+          font-weight: bold;
+        }
+        .status{
+          width: 50%;
+          height: min-content;
+        }
+      }
+    }
   }
+  .product {
+    width: 65%;
+    .button {
+      background: #4caf50 !important;
+      color: #fff;
+    }
+  }
+}
 </style>
